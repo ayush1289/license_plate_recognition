133703952374671fa0ee8210fa346f22c5a51287
ayush1289 <ayushshankarpurkar11@gmail.com>
2023-02-17 01:32:50 +0530

    fix: added comments and removed extra lines of code

--- Number_plate_detection.py
+++ Number_plate_detection.py
@@ -1,18 +1,19 @@
+# importing modules
 import cv2
 import numpy as np
 
-# frameWidth = 640    #Frame Width
-# franeHeight = 480   # Frame Height
-
+# cascade classifier used to detect rectangle or license plate
 plateCascade = cv2.CascadeClassifier("haarcascade_russian_plate_number.xml")
 minArea = 500
-count = "2"
-img =cv2.imread(f"dataset_lp/{count}.jpeg")
+
+
+count = "3"
+# cv2.imread is used to read the image file.
+img = cv2.imread(f"dataset_lp/{count}.jpeg")
 
 name = f"img_{count}"
 
 while True:
-    
 
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -22,16 +23,15 @@ while True:
         area = w*h
         if area > minArea:
             cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            cv2.putText(img,"NumberPlate",(x,y-5),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
-            imgRoi = img[y:y+h,x:x+w]
-            cv2.imshow("ROI",imgRoi)
-    cv2.imshow("Result",img)
-    if cv2.waitKey(1) & 0xFF ==ord('q'):
-        # cv2.imwrite("images_lp/"+f"{name}"+".jpg",imgRoi)
-        # cv2.rectangle(img,(0,200),(640,300),(0,255,0),cv2.FILLED)
-        cv2.putText(img,"Scan Saved",(15,265),cv2.FONT_HERSHEY_COMPLEX,2,(0,0,255),2)
-        cv2.imshow("Result",img)
-        cv2.waitKey(500)
+            cv2.putText(img, "NumberPlate", (x, y-5),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+            imgRoi = img[y:y+h, x:x+w]
+            cv2.imshow("ROI", imgRoi)
+    cv2.imshow("Result", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imwrite("images_lp/"+f"{name}"+".jpg", imgRoi)
+        cv2.imshow("Result", img)
+        # cv2.waitKey(500)
         break
 cv2.destroyAllWindows()
-        # count+=1
+# count+=1
--- license_to_text.py
+++ license_to_text.py
@@ -3,23 +3,25 @@ import numpy as np
 import pytesseract
 
 # Load the image
-img = cv2.imread("images_lp/img_1.jpg")
-
+img = cv2.imread("images_lp/img_11.jpg")
+# img = cv2.resize(img,(300,150))
 # Convert to hsv
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 # Get the binary mask
-msk = cv2.inRange(hsv, np.array([0, 0, 0]), np.array([179, 255, 154]))
+msk = cv2.inRange(hsv, np.array([0, 0, 0]), np.array([180, 255, 154]))
 
 # Extract
-krn = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 3))
-dlt = cv2.dilate(msk, krn, iterations=5)
+krn = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+dlt = cv2.dilate(msk, krn, iterations=5,)
 res = 255 - cv2.bitwise_and(dlt, msk)
 
 # OCR
-txt = pytesseract.image_to_string(res, config="--psm 6")
+txt = pytesseract.image_to_string(res, config="--psm 7").replace(" ","")
 print(txt)
 
 # Display
 cv2.imshow("res", res)
 cv2.waitKey(0)
+
+
